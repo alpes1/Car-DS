6363a1d2d4780098ea0256f86448cee1ec5502b4
alpes1 <ibra.acht2002@gmail.com>
2025-01-19 23:17:25 +0100

    "First commit "

---
+++ src/test/java/com/pfa/dscargraphql/service/CarManagerTest.java
@@ -0,0 +1,20 @@
+package com.pfa.dscargraphql.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarManagerTest {
+
+    @Test
+    void getAllCars() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getCarByModel() {
+    }
+}
